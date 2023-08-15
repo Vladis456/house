5ad2f3db86ea033860f367aeebaf317bb05c7f2d
Vladis456 <m.obey@bk.ru>
2023-08-15 21:42:36 +0300

    adding dist

--- src/Components/layout/Container/Container.jsx
+++
@@ -1,7 +0,0 @@
-import style from './Container.module.scss'
-import cn from 'classnames'
-export const Container = ({className, children}) => {
-	return <div className={cn(style.container, className)}>
-		{children}
-	</div>
-}
